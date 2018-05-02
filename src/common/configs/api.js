876ad3896c4f309d993edf7508b913080036efdf
wklc2014 <373658358@qq.com>
2018-05-03 06:53:03 +0800

    [master]

--- src/common/configs/api.js
+++ src/common/configs/api.js
@@ -5,3 +5,4 @@ const domain = 'http://10.63.68.129:15000/learn-antd';
 export const example = `${domain}/example`;
 export const getUser = `${domain}/user`;
 export const fetch = `${domain}/fetch`;
+
