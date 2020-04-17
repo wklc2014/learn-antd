4ec8e131f60d160e7184d90a333c80bd7664b67f
wb-wk228937 <wb-wk228937@ant-financial.com>
2020-04-17 19:25:54 +0800

    add antd-with

---
+++ src/components/antd-with/with-modal.js
@@ -0,0 +1,18 @@
+/**
+ * Modal 高阶组件
+ */
+import React, { Component } from 'react';
+import { Modal } from 'antd';
+
+export default function withModal(WraperComponent) {
+
+  return class extends Component {
+
+    defaultProps = {
+
+    }
+
+    return
+
+  }
+}
